05366f3e0262f25ad8c30b10d160f0fcfaa2a03c
Masaki <anthrgrnwrld@gmail.com>
2020-10-27 20:50:38 +0900

    Initial Commit

---
+++ Subject002/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Subject002
+//
+//  Created by Masaki Horimoto on 2020/10/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
