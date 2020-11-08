e43911910f636276a3e75d9babef7a3da95851a2
Masaki <anthrgrnwrld@gmail.com>
2020-11-09 07:36:12 +0900

    あきおさん版

--- Subject002/ViewController.swift
+++ Subject002/ViewController.swift
@@ -7,152 +7,98 @@
 
 import UIKit
 
+/// 計算で扱う2つの値を保持する責務のみ持つ
+struct CalculatingSource {
+    let value1: Int
+    let value2: Int
+}
+
+
+/// 2つの値に対する計算に関する責務のみ受け持つ
+enum Operator: Int {
+    case add
+    case subtract
+    case multiply
+    case devide
+
+    enum Error: Swift.Error {
+        case dividingByZero
+    }
+
+    func calculate(source: CalculatingSource) throws -> Int {
+        switch self {
+        case .add, .subtract, .multiply:
+            break
+        case .devide:
+            guard source.value2 != 0 else {
+                throw Error.dividingByZero
+            }
+        }
+
+        return ope(source.value1, source.value2)
+    }
+
+    private var ope: (Int, Int) -> Int {
+        switch self {
+        case .add:
+            return (+)
+        case .subtract:
+            return (-)
+        case .multiply:
+            return (*)
+        case .devide:
+            return (/)
+        }
+    }
+}
+
 class ViewController: UIViewController {
     
-    @IBOutlet var numTextField: [UITextField]!
-    @IBOutlet weak var resultCalc: UILabel!
+    @IBOutlet var numTextFields: [UITextField]!
+    @IBOutlet weak var resultCalcLabel: UILabel!
     @IBOutlet weak var operatorSegment: UISegmentedControl!
     
     private enum ErrorSubject2: Error {
         case text2IntError
     }
 
-    class Calculate {
-        
-        enum Operator: String, CaseIterable {
-            case add = "+"
-            case subtract = "-"
-            case multiply = "×"
-            case devide = "÷"
-        }
-        
-        enum ErrorCalculate: Error {
-            case zeroDevide
-        }
-        
-//        let operatorChar = [Operator.add.rawValue,
-//                            Operator.subtract.rawValue,
-//                            Operator.multiply.rawValue,
-//                            Operator.devide.rawValue]
-        
-        func getOperator(opText: String) -> Operator? {
-//            switch(opText) {
-//                case Operator.add.rawValue:
-//                    return Operator.add
-//                case Operator.subtract.rawValue:
-//                    return Operator.subtract
-//                case Operator.multiply.rawValue:
-//                    return Operator.multiply
-//                case Operator.devide.rawValue:
-//                    return Operator.devide
-//                default:
-//                    return nil
-//            }
-            Operator(rawValue: opText)
-        }
-        
-        func calc(op: Operator, num1: Int?, num2: Int?) throws -> Int {
-            
-            if num1 == nil || num2 == nil {
-                fatalError("num is nil.")
-            }
-            let num1 = num1!
-            let num2 = num2!
-            
-            let result: Int
-            switch op {
-                case .add:
-                    result = num1 + num2
-                case .subtract:
-                    result = num1 - num2
-                case .multiply:
-                    result = num1 * num2
-                case .devide:
-                    if num2 == 0 {
-                        throw ErrorCalculate.zeroDevide
-                    }
-                    result = num1 / num2
-            }
-            return result
-        }
-        
-    }
-    
-    let calcSubject002 = Calculate()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        //SegmentedControlの表示文字列をセット
-//        calcSubject002.operatorChar.enumerated().forEach{
-        Calculate.Operator.allCases.enumerated().forEach{
-//            operatorSegment.setTitle($0.1.rawValue, forSegmentAt: $0.0)
-            operatorSegment.setTitle($0.1.rawValue, forSegmentAt: $0.0)
-        }
-    }
-
     //ボタンを押した時
     @IBAction func pressButton(_ sender: Any) {
         
-        //テキストフィールドの文字列を数値に変換する
-        let numArray = changeNumTextFieldArrayToInt(numTextField)
-        
-        //テキストフィールドに有効な数字が入っている場合のみ計算を実行する
-        if !canCalculate(numArray) {
+        guard let calculatingSource = InputNumberExtractor().extract(numTextFields: numTextFields) else {
+            resultCalcLabel.text = "各枠に数字（半角）を入力して下さい"
             return
         }
-        
-        let selectedIndex = operatorSegment.selectedSegmentIndex    //選択中のセグメントのIndexを取得する
-        guard let opText = operatorSegment.titleForSegment(at: selectedIndex) else {    //洗濯中のセグメントのテキストを取得
-            fatalError("Can't get value of titleForSegment.")
+
+        guard let ope = Operator(rawValue: operatorSegment.selectedSegmentIndex) else {
+            fatalError("selectedSegmentIndex is invalid.")
         }
-        guard let op = calcSubject002.getOperator(opText: opText) else { //洗濯中の演算子を取得
-            fatalError("Can't get value of titleForSegment.")   //テキストが+,-,×,÷以外の時には何かがおかしいのでfatalError
-        }
-        
-        //計算、及び、ラベルへ結果の出力
-        let outputText: String
+
         do {
-            let result = try calcSubject002.calc(op: op, num1: numArray[0], num2: numArray[1])
-            outputText = "\(result)"
+            resultCalcLabel.text = String(try ope.calculate(source: calculatingSource))
+        } catch Operator.Error.dividingByZero {
+            resultCalcLabel.text = "割る数には0以外を入力して下さい"
         } catch {
-            outputText = "割る数には0以外を入力して下さい"
+
         }
         
-        resultCalc.text = outputText
-    }
-    
-    //テキストフィールドの文字列を数値に変換する関数
-    private func changeNumTextFieldArrayToInt(_ numTextField: [UITextField]) -> ([Int?]){
-        let numArray: [Int?] = numTextField.map {
-            do {
-                let text = $0.text ?? ""
-                if Int(text) == nil {
-                    throw ErrorSubject2.text2IntError
-                } else {
-                    return Int(text)!
-                }
-            } catch {
-                resultCalc.text = "各枠に数字（半角）を入力して下さい"
-                return nil
-            }
-        }
-        return numArray
-    }
-    
-    //計算可能な数値が入っているか判断する関数
-    private func canCalculate(_ numArray: [Int?]) -> Bool {
-        let retValue: Bool = numArray.map {
-            if $0 == nil {
-                return false
-            } else {
-                return true
-            }
-        }.reduce(true) {
-            $0 == false || $1 == false ? false : true
-        }
-        return retValue
     }
 
+}
+
+
+/// テキストフィールドから2つの値を取り出すことに関する責務のみ持つ
+struct InputNumberExtractor {
+    func extract(numTextFields: [UITextField]) -> CalculatingSource? {
+        let numbers = numTextFields
+            .map { $0.text ?? "" }
+            .compactMap { Int($0) }
+
+        guard numbers.count == 2 else {
+            return nil
+        }
+
+        return CalculatingSource(value1: numbers[0], value2: numbers[1])
+    }
 }
 
